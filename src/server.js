c07a33c5ee96963a89625f7c5a7683ebdccca2dd
gavrakr <gavrakr@naver.com>
2024-12-19 00:59:03 +0900

    2024-12-19-00:58

---
+++ src/server.js
@@ -0,0 +1,56 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const urlLogger = (req, res, next) => {
+  console.log(`PATH: ${req.url}`);
+  next();
+};
+
+const timeLogger = (req, res, next) => {
+  const date = new Date();
+  const year = console.log(
+    `Time: ${date.getFullYear()}.${date.getMonth()}.${date.getDate()}`
+  );
+  next();
+};
+
+const securityLogger = (req, res, next) => {
+  const protocol = req.protocol;
+  if (protocol === "https") {
+    console.log("secure");
+  } else {
+    console.log("insecure");
+  }
+  next();
+};
+
+const protectorMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(urlLogger);
+app.use(timeLogger);
+app.use(securityLogger);
+app.use(protectorMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
