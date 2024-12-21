2e23c39f051fc70bc3e5693da5e699773d8c5cb9
seoungwon <gavrakr@naver.com>
2024-12-21 17:25:42 +0900

    2024-12-21-17:25

---
+++ src/routers/index.js
@@ -0,0 +1,7 @@
+// import globalRouter from "./globalRouter";
+// import userRouter from "./userRouter";
+// import videoRouter from "./videoRouter";
+
+export { default as globalRouter } from "./globalRouter";
+export { default as userRouter } from "./userRouter";
+export { default as videoRouter } from "./videoRouter";
--- src/server.js
+++ src/server.js
@@ -1,57 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import { globalRouter, userRouter, videoRouter } from "./routers";
 
 const PORT = 4000;
 
 const app = express();
+
+/// LOG ///
 const logger = morgan("dev");
-
-// const urlLogger = (req, res, next) => {
-//   console.log(`PATH: ${req.url}`);
-//   next();
-// };
-
-// const timeLogger = (req, res, next) => {
-//   const date = new Date();
-//   const year = console.log(
-//     `Time: ${date.getFullYear()}.${date.getMonth()}.${date.getDate()}`
-//   );
-//   next();
-// };
-
-// const securityLogger = (req, res, next) => {
-//   const protocol = req.protocol;
-//   if (protocol === "https") {
-//     console.log("secure");
-//   } else {
-//     console.log("insecure");
-//   }
-//   next();
-// };
-
-const protectorMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.end();
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-
 app.use(logger);
-// app.use(urlLogger);
-// app.use(timeLogger);
-// app.use(securityLogger);
-app.use(protectorMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
